e266d84ed38d41de472375963399f183b785347b
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2017-03-22 21:01:55 +0100

    Fixup

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ runcommand() {
 
 # create passwd entry for current uid, fix HOME variable
 # only execute, if the current uid does not exist.
-if id $(id -u) 2>/dev/null then
+if id $(id -u) 2>/dev/null; then
   echo "Not creating user with uid $(id -u) because he already exists"
 else
   create_user $(id -u)
